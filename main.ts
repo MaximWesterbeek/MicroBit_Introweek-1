f7309b00776fd2be6b9b64021f1bf7ca410e0923
MaximWesterbeek <114074573+MaximWesterbeek@users.noreply.github.com>
2022-09-21 15:43:53 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (input.lightLevel() < 75) {
+        if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -60) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+radio.setGroup(175)
